c1a3df95104310d77b35749f447282c581483eaa
James Broski <james.broski.5@gmail.com>
2021-08-10 16:45:12 -0600

    troubleshooting with API

---
+++ app.js
@@ -0,0 +1,21 @@
+var APIkey = " d07b9275bb45428d6f5ad27aa9152b94 ";
+
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputValue");
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=d07b9275bb45428d6f5ad27aa9152b94"
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+);
+
+button.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=d07b9275bb45428d6f5ad27aa9152b94"
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  );
+});
