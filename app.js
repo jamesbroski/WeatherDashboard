3b18f61c187adb78b8c3a2ff9bc2f3073461a3f1
James Broski <james.broski.5@gmail.com>
2021-08-10 17:14:45 -0600

    working API linked

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ var name = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
 
-fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=d07b9275bb45428d6f5ad27aa9152b94"
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-);
-
 button.addEventListener("click", function () {
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=d07b9275bb45428d6f5ad27aa9152b94"
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-  );
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      inputValue.value +
+      "&appid=d07b9275bb45428d6f5ad27aa9152b94"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 });
